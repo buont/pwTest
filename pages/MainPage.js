277dd05952f3410e79cc69a4d50bf00a2e22dfd0
dariusz.bzdyk <dariusz.bzdyk@xperi.com>
2025-05-11 20:15:09 +0200

    delete not used locator

--- pages/MainPage.js
+++ pages/MainPage.js
@@ -2,7 +2,6 @@ export default class MainPage{
     constructor(page) {
         this.page = page;
         this.profileLink = page.locator('a.nav-link[href^="/profile/"]');
-        this.newArticle = page.locator('a.nav-link[href^="/editor"]');
     }
 
 
